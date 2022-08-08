64c92264962024a9b761da72c583e089563f3b09
atticusf08 <atticusfran@gmail.com>
2022-08-08 12:05:57 -0400

    Create hi.cpp

---
+++ hi.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdio.h>
+#include <cmath>
+#include <iomanip>
+
+int main()
+{
+    std::cout << "test to see if my github repository, musical-octo-garbanzo, is working";
+
+    return 0;
+}
